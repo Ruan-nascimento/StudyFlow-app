f48b7076cd1fd936ee22e1825b41e3dfd4694ad7
Ruan Carlos <ruan.nascimento019@gmail.com>
2025-08-28 14:05:07 -0300

    refactor(app): reorganize project structure and update dependencies
    - Move App component to src/app directory and update imports
    - Replace babel.config.ts with babel.config.js for Expo compatibility
    - Add global styles and update App component to use them
    - Add babel-preset-expo dependency for React Native support

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, ViewStyle } from 'react-native';
+import { globalStyles } from '../styles/globals.styles';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={globalStyles.container as ViewStyle}>
+      <Text>Meu Primeiro App</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
